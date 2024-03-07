2ab25e8594abaf55beb991cc75b534c8ed0a0da1
Kisselny <admin@MacBook-LizDim.local>
2024-03-07 13:06:00 +0300

    moar tests

--- AreaCalcLib/ShapeClasses/Circle.cs
+++ AreaCalcLib/ShapeClasses/Circle.cs
@@ -7,9 +7,13 @@ namespace AreaCalcLib;
 /// Use it to create Circle objects (you need to provide
 /// radius to the constructor) and access its Area property,
 /// or call the CalculateArea method to get the area value statically.
+/// It uses PI value approximated
+/// to 10 digits after dot (3.1415926535).
 /// </summary>
 public class Circle : IAreaCalculation
 {
+    private const double _pi = 3.1415926535;
+    
     /// <summary>
     /// Cicrle's area property.
     /// </summary>
@@ -32,8 +36,8 @@ public class Circle : IAreaCalculation
     /// <returns></returns>
     public static double CalculateArea(double radius)
     {
-        double circleArea = System.Math.Pow(radius, 2) * System.Math.PI;
-        return circleArea;
+        double circleArea = radius * radius * _pi;
+        return Math.Round(circleArea, 10);
     }
     
 }
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74bfbb7482c46dda98c5bd33aa6d19095cf90204")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
